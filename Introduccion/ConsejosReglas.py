ee064171361d85e8ecf48b552f04f8e779fdc4aa
jose1805 <jvillalobos@flising.com>
2023-09-29 17:46:32 -0600

    consejos y reglas

---
+++ Introduccion/ConsejosReglas.py
@@ -0,0 +1,11 @@
+# No se pueden usar palabras reservadas
+import keyword
+print(keyword.kwlist)
+
+# Caracteres especiales no son validos 
+# Sin espacios
+# No se pueden declarar variables iniciando con numero el nombre
+
+# nombres en variables descriptivas
+# usar variables capitalizadas UsuarioSistemaTemporal
+# se puede usar _
